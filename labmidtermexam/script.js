478dfa15b67ff3f855c453aef8f6ab08d93a31e6
sp23-bse-037 <amnamansoorpk@gmail.com>
2024-10-22 16:18:10 +0500

    Finalmidexam

--- labmidtermexam/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('profile').addEventListener('mouseover', function() {
-    document.getElementById('intro').style.opacity = '1';
-});
-
-document.getElementById('profile').addEventListener('mouseout', function() {
-    document.getElementById('intro').style.opacity = '0';
-});
